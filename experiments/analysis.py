5704f3b305b60d3ffa41fde52ad19e020a7567d7
Brandon Thayer <blthayer@tamu.edu>
2020-01-28 08:32:09 -0600

    Missed the draft of analysis.py on previous commit.

---
+++ experiments/analysis.py
@@ -0,0 +1,33 @@
+# noinspection PyUnresolvedReferences
+from constants import THIS_DIR
+import pandas as pd
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def main():
+    df = pd.read_csv(
+        os.path.join(THIS_DIR, 'gridmind_reproduce', 'log_train.csv'))
+
+    # "line" graph for action_taken.
+    # Relatively useless
+    # plt.plot(df['action_taken'])
+
+    # "bar" graph of all actions
+    action_count = df['action_taken'].value_counts(
+        normalize=False, sort=False, ascending=False, dropna=True)
+    action_count_norm = df['action_taken'].value_counts(
+        normalize=True, sort=True, ascending=False, dropna=True
+    )
+    # Still have to sort it? Fine.
+    action_count.sort_index(inplace=True)
+    # Bar graph.
+    #
+    plt.bar(x=action_count.index, height=action_count.to_numpy())
+    plt.show()
+    pass
+
+
+if __name__ == '__main__':
+    main()
