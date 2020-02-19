ab00da3c38c343d7ef24d3b62924b27cb4000803
Brandon Thayer <blthayer@tamu.edu>
2020-02-19 17:15:11 -0600

    Adding the 200 bus case.

--- cases/README.md
+++ cases/README.md
@@ -3,3 +3,7 @@ Directory to store PowerWorld cases.
 
 ## ieee_14
 IEEE 14 bus model. See README inside for more details.
+
+## il_200
+Synthetic 200 bus case representing a portion of central Illinois (but
+fictional!). Downloaded from [here](https://electricgrids.engr.tamu.edu/electric-grid-test-cases/activsg200/).
--- experiments/constants.py
+++ experiments/constants.py
@@ -6,6 +6,7 @@ import numpy as np
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 CASE_DIR = os.path.abspath(os.path.join(THIS_DIR, '..', 'cases'))
 IEEE_14_DIR = os.path.join(CASE_DIR, 'ieee_14')
+IL_200_DIR = os.path.join(CASE_DIR, 'il_200')
 
 # Constants for IEEE 14 bus files.
 IEEE_14_PWB = os.path.join(IEEE_14_DIR, 'IEEE 14 bus.pwb')
@@ -13,6 +14,8 @@ IEEE_14_PWB_CONDENSERS = os.path.join(IEEE_14_DIR,
                                       'IEEE 14 bus condensers.PWB')
 IEEE_14_ONELINE_AXD = os.path.join(IEEE_14_DIR, 'IEEE 14 bus.axd')
 IEEE_14_CONTOUR_AXD = os.path.join(IEEE_14_DIR, 'contour.axd')
+
+IL_200_PWB = os.path.join(IL_200_DIR, 'ACTIVSg200.pwb')
 
 # Write to a faster SSD to make life better.
 DATA_DIR = r'D:\\runs'
