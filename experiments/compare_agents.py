1c854c1c54a6754e0ea5222123d978f7bbea7e09
Brandon Thayer <blthayer@tamu.edu>
2020-02-27 11:55:14 -0600

    Updating compare_agents.py to use an object-oriented approach for the graph agent, which should be faster. IMPORTANT NOTE: graph agent episodes are terminated early if the agent takes the no-op action. This will skew rewards for reward scheme 1.

--- experiments/compare_agents.py
+++ experiments/compare_agents.py
@@ -20,6 +20,7 @@ import pickle
 import networkx as nx
 import re
 import argparse
+from typing import Union
 
 from gym_powerworld.envs.voltage_control_env import V_TOL
 
@@ -113,35 +114,22 @@ def main(case_str, random, mod, env_name, clipped_r, seed):
         if random:
             env.action_space.seed(seed=seed)
         else:
-            # Extract nodes with generators. This should come back
-            # sorted since the gen_init_data comes back from ESA sorted
-            # by BusNum.
-            gen_buses = env.gen_buses.to_numpy()
+            # Initialize a graph agent.
+            g_agent = GraphAgent(env=env, file_str=fs, random=random)
 
-            # For the graph technique, we'll be setting gens to the
-            # highest and second highest allowable voltage. Take
-            # advantage of this fact, and compute the starting action
-            # numbers for these voltage set points. The + 1 is due to
-            # the no-op action.
-            starting_action_num_105 = \
-                env.num_gen_reg_buses * (env.gen_bins.shape[0] - 1) + 1
-
-            starting_action_num_1025 = \
-                env.num_gen_reg_buses * (env.gen_bins.shape[0] - 2) + 1
-
+        # Initialize array to track success.
+        success = np.zeros(TEST_EPISODES)
         # Loop and take random actions.
         action_list = []
-        for _ in range(TEST_EPISODES):
-            obs = env.reset()
+
+        for i in range(TEST_EPISODES):
+            _ = env.reset()
             done = False
 
             if not random:
-                # Get buses which have a generator that is on.
+                # Prep the graph agent.
                 # noinspection PyUnboundLocalVariable
-                gens_on = gen_buses[env.gen_bus_status_arr]
-
-                # Get a graph for this episode.
-                graph = get_network_graph(env, fs, random)
+                g_agent.init_episode()
 
             # Clear the action list.
             action_list.clear()
@@ -158,180 +146,277 @@ def main(case_str, random, mod, env_name, clipped_r, seed):
                             action = get_random_action(env)
 
                 else:
-                    # Use the graph technique to get an action. Note
-                    # that we're not going to use the observation
-                    # returned by the environment, and instead use the
-                    # raw per unit voltages.
-                    # noinspection PyUnboundLocalVariable
-                    action = get_graph_action(
-                        env=env, graph=graph, obs=env.bus_pu_volt_arr,
-                        gens_on=gens_on,
-                        starting_action_num_105=starting_action_num_105,
-                        starting_action_num_1025=starting_action_num_1025,
-                        action_list=action_list
-                    )
+                    # Use the graph technique to get an action.
+                    action = g_agent.get_action(action_list)
 
                 # Put the action in the action list.
                 action_list.append(action)
 
                 # Perform the step.
-                obs, _, done, _ = env.step(action)
+                _, _, done, info = env.step(action)
+
+                # TODO/NOTE: IMPORTANT! Breaking the loop early if the
+                #   graph agent takes the no-op action. This will give
+                #   the graph agent an artificially high reward, so
+                #   it's important we NOT compare rewards and only
+                #   compare success rates.
+                if (not random) and (action == env.no_op_action):
+                    # Since we're hacking around things, the environment
+                    # hasn't actually filled in the 'is_success' field
+                    # if we aren't done.
+                    if not done:
+                        info['is_success'] = False
+                    done = True
+
+            # noinspection PyUnboundLocalVariable
+            success[i] = info['is_success']
 
         # Close environment, which flushes log.
         env.close()
 
+        suc_pct = success.sum() / success.shape[0] * 100
+        print(f'Success percentage: {suc_pct:.2f}%')
+
+
+class GraphAgent:
+    """Simple graph-based agent."""
+
+    def __init__(self, env: gym_powerworld.envs.DiscreteVoltageControlEnv,
+                 file_str, random):
+        # Track environment.
+        self.env = env
+
+        # File string and random are used to make a unique Y-bus string.
+        self.ybus_file = os.path.join(
+            THIS_DIR, f'ybus_{file_str}_{int(random)}.mat')
+
+        # Extract nodes with generators. This should come back
+        # sorted since the gen_init_data comes back from ESA sorted
+        # by BusNum.
+        self.gen_buses = self.env.gen_buses.to_numpy()
+
+        # Extract nodes with shunts.
+        # TODO: IMPORTANT! This will NOT WORK when there are multiple
+        #   shunts at a single bus. It'll work for now with the 200 and
+        #   500 bus cases which do not have multiple shunts at the
+        #   same buses. The fix is to do something similar inside the
+        #   gym-powerworld repo as was done with generators.
+        #   Multi-indexing and such.
+        if self.env.num_shunts > 0:
+            self.shunt_buses = self.env.shunt_init_data['BusNum'].to_numpy()
+        else:
+            self.shunt_buses = None
+
+        # For the graph technique, we'll be setting gens to the
+        # highest and second highest allowable voltage. Take
+        # advantage of this fact, and compute the starting action
+        # numbers for these voltage set points. The + 1 is due to
+        # the no-op action.
+        self.starting_action_num_105 = \
+            self.env.num_gen_reg_buses * (self.env.gen_bins.shape[0] - 1) + 1
+
+        self.starting_action_num_1025 = \
+            self.env.num_gen_reg_buses * (self.env.gen_bins.shape[0] - 2) + 1
+
+        # The following will be configured in the init_episode method.
+        self.gens_on: Union[np.ndarray, None] = None
+        self.shunts_on: Union[np.ndarray, None] = None
+        self.graph: Union[nx.Graph, None] = None
+        self.distance_indices = []
+        self.prev_bus_num = None
+        self.prev_distances: Union[np.ndarray, None] = None
+        self.prev_distances_argsort = None
+
+    def init_episode(self):
+        """Perform initialization work required for an episode."""
+        self.gens_on = self.gen_buses[self.env.gen_bus_status_arr]
+        if self.shunt_buses is not None:
+            self.shunts_on = self.shunt_buses[self.env.shunt_status_arr]
+        self.graph = self.get_network_graph()
+        self.distance_indices.clear()
+        self.prev_bus_num = None
+        self.prev_distances = None
+        self.prev_distances_argsort = None
+
+    def get_action(self, action_list):
+        # Before proceeding any further, check to see if all voltages are
+        # already in bounds. If this is the case, take the no-op action.
+        if self.env.all_v_in_range:
+            return 0
+
+        # Pull the bus voltages from the environment.
+        obs = self.env.bus_pu_volt_arr
+
+        # Get lowest and highest magnitude voltage buses, recalling that
+        # the bus voltages always go first in the observation. Bump by 1 due
+        # to 0-based indexing.
+        low_bus = int(np.argmin(obs) + 1)
+        low_v = obs[low_bus - 1]
+        high_bus = int(np.argmax(obs) + 1)
+        high_v = obs[high_bus - 1]
+
+        # Set flag for whether or not we're going to work on the lowest
+        # voltage or the highest voltage.
+        low_flag = low_v < (0.95 + V_TOL)
+
+        # Determine which bus to get electrical distances from, and which
+        # action number to use in computing the action to take.
+        if low_flag:
+            bus = low_bus
+            starting_action_num = self.starting_action_num_105
+            v = 1.05
+        elif high_v > (1.05 + V_TOL):
+            bus = high_bus
+            starting_action_num = self.starting_action_num_1025
+            v = 1.025
+        else:
+            # We should never get here.
+            return 0
+
+        # Get electrical distances from the bus to each generator (reactance
+        # only). Skip if we're looking at the same bus.
+        if bus == self.prev_bus_num:
+            distances = self.prev_distances
+            distances_argsort = self.prev_distances_argsort
+        else:
+            distances = np.zeros(self.gens_on.shape[0])
+            for idx in range(self.gens_on.shape[0]):
+                try:
+                    d = nx.dijkstra_path_length(
+                        self.graph, bus, self.gens_on[idx], 'x')
+                except nx.exception.NetworkXNoPath:
+                    # If for whatever reason there's no path, set the
+                    # distance to infinity.
+                    d = np.inf
+                    print('No path!')
+
+                distances[idx] = d
+
+            # Track bus and distances to possibly save computation in the
+            # next step.
+            self.prev_bus_num = bus
+            self.prev_distances = distances
+            distances_argsort = np.argsort(distances)
+            self.prev_distances_argsort = distances_argsort
+
+        # Loop to determine a valid action. Initialize
+        # to the no-op action.
+        action = 0
+        for idx in distances_argsort:
+            # Skip this generator if the distance is infinite.
+            if np.isinf(distances[idx]):
+                continue
+
+            # Get the bus number corresponding to this
+            # generator.
+            gen_bus = self.gens_on[idx]
+
+            # noinspection PyUnresolvedReferences
+            gen_mask = \
+                (self.env.gen_obs_data['BusNum'] == gen_bus).to_numpy()
+
+            # Look up the voltage set point.
+            v_set = \
+                self.env.gen_obs_data['GenVoltSet'].to_numpy()[
+                    gen_mask]
+            # There can be multiple generators at a bus. The environments
+            # should command them all to the same set point. Sanity check
+            # that here.
+            assert np.allclose(v_set[0], v_set)
+            v_set = v_set[0]
+
+            # If the set point is already at the appropriate setting, or
+            # move on to the next generator.
+            if np.isclose(v_set, v, rtol=0.0, atol=V_TOL):
+                continue
+
+            # Put this generator index in the list so we can skip it
+            # next time.
+            self.distance_indices.append(idx)
+
+            # Determine what action we need.
+            action = starting_action_num + np.argmax(gen_mask)
+
+            # If this action has already been taken this episode, keep on
+            # looping.
+            if action in action_list:
+                continue
+            else:
+                # We have an action to take. Break the loop.
+                break
+
+        return action
+
+    def get_network_graph(self):
+        # Initialize a graph.
+        g = nx.Graph()
+
+        # Save YBus.
+        self.env.saw.RunScriptCommand(
+            f'SaveYbusInMatlabFormat("{self.ybus_file}", NO)')
+
+        # Load YBus.
+        with open(self.ybus_file, 'r') as f1:
+            # Pass on the first two lines. They contain something like:
+            # j = sqrt(-1);
+            # Ybus = sparse(14);
+            f1.readline()
+            f1.readline()
+            # Read the rest.
+            mat_str = f1.read()
+
+        # Remove whitespace from the string.
+        mat_str = re.sub('\s', '', mat_str)
+
+        # Split by semicolons.
+        entries = re.split(';', mat_str)
+
+        # Loop. The last entry is the empty string, so exclude it.
+        for e in entries[:-1]:
+            # Split about the equals sign.
+            eq_split = re.split('=', e)
+
+            # The first part is going to be related to the index, the second
+            # part related to the value.
+            idx_str = eq_split[0]
+            value_str = eq_split[1]
+
+            # Pull the values out of the string. This is crude, but works.
+            idx = [int(x) for x in
+                   re.split(',',
+                            re.match('(?:Ybus\()(.*)(?:\))', idx_str).group(
+                                1))]
+
+            # Not working with diagonals.
+            if idx[0] == idx[1]:
+                continue
+
+            # Remove parentheses from the value_str.
+            value_str = re.sub('(?:\()(.*)(?:\))', lambda m: m.group(1),
+                               value_str)
+
+            # Split by the '+j*'
+            val_list = [float(x) for x in re.split('\+j\*', value_str)]
+
+            # Create complex number, invert and multiply by -1 to convert
+            # admittance to impedance.
+            try:
+                cplx = -1 / (val_list[0] + 1j * val_list[1])
+            except ZeroDivisionError:
+                # Move along if the buses are not connected.
+                continue
+
+            # Extract the reactance.
+            x = cplx.imag
+
+            # Add to the graph.
+            g.add_edge(idx[0], idx[1], x=x)
+
+        return g
+
 
 def get_random_action(env):
     return env.action_space.sample()
-
-
-def get_graph_action(env, graph, obs, gens_on, starting_action_num_105,
-                     starting_action_num_1025, action_list):
-    # Before proceeding any further, check to see if all voltages are
-    # already in bounds. If this is the case, take the no-op action.
-    if env.all_v_in_range:
-        return 0
-
-    # Get lowest and highest magnitude voltage buses, recalling that
-    # the bus voltages always go first in the observation. Bump by 1 due
-    # to 0-based indexing.
-    low_bus = int(np.argmin(obs[0:env.num_buses]) + 1)
-    low_v = obs[0:env.num_buses][low_bus - 1]
-    high_bus = int(np.argmax(obs[0:env.num_buses]) + 1)
-    high_v = obs[0:env.num_buses][high_bus - 1]
-
-    # Set flag for whether or not we're going to work on the lowest
-    # voltage or the highest voltage.
-    low_flag = low_v < (0.95 + V_TOL)
-
-    # Determine which bus to get electrical distances from, and which
-    # action number to use in computing the action to take.
-    if low_flag:
-        bus = low_bus
-        starting_action_num = starting_action_num_105
-        v = 1.05
-    elif high_v > (1.05 + V_TOL):
-        bus = high_bus
-        starting_action_num = starting_action_num_1025
-        v = 1.025
-    else:
-        # We should never get here.
-        return 0
-
-    # Get electrical distances from the bus to each generator (reactance
-    # only).
-    distances = \
-        np.array([nx.dijkstra_path_length(graph, bus, gen_bus, 'x')
-                  for gen_bus in gens_on])
-
-    # Loop to determine a valid action. Initialize
-    # to the no-op action.
-    action = 0
-    for idx in np.argsort(distances):
-        # Get the bus number corresponding to this
-        # generator.
-        gen_bus = gens_on[idx]
-
-        # noinspection PyUnresolvedReferences
-        gen_mask = \
-            (env.gen_obs_data['BusNum'] == gen_bus).to_numpy()
-
-        # Look up the voltage set point.
-        v_set = \
-            env.gen_obs_data['GenVoltSet'].to_numpy()[
-                gen_mask]
-        # There can be multiple generators at a bus. The environments
-        # should command them all to the same set point. Sanity check
-        # that here.
-        assert np.allclose(v_set[0], v_set)
-        v_set = v_set[0]
-
-        # If the set point is already at the appropriate setting, or
-        # move on to the next generator.
-        if np.isclose(v_set, v, rtol=0.0, atol=V_TOL):
-            continue
-
-        # Determine what action we need.
-        action = starting_action_num + np.argmax(gen_mask)
-
-        # If this action has already been taken this episode, keep on
-        # looping.
-        if action in action_list:
-            continue
-        else:
-            # We have an action to take. Break the loop.
-            break
-
-    return action
-
-
-def get_network_graph(env, file_str, random):
-    """Get a graph representing the PowerWorld case. The resulting graph
-    will have edges with an 'x' attribute for reactance (float), and
-    nodes will have a 'gen' attribute (True or False)
-    """
-    # Initialize a graph.
-    g = nx.Graph()
-
-    # Save YBus.
-    f = os.path.join(THIS_DIR, f'ybus_{file_str}_{int(random)}.mat')
-    env.saw.RunScriptCommand(f'SaveYbusInMatlabFormat("{f}", NO)')
-    # Load YBus.
-    with open(f, 'r') as f1:
-        # Pass on the first two lines. They contain something like:
-        # j = sqrt(-1);
-        # Ybus = sparse(14);
-        f1.readline()
-        f1.readline()
-        # Read the rest.
-        mat_str = f1.read()
-
-    # Remove whitespace from the string.
-    mat_str = re.sub('\s', '', mat_str)
-
-    # Split by semicolons.
-    entries = re.split(';', mat_str)
-
-    # Loop. The last entry is the empty string, so exclude it.
-    for e in entries[:-1]:
-        # Split about the equals sign.
-        eq_split = re.split('=', e)
-
-        # The first part is going to be related to the index, the second
-        # part related to the value.
-        idx_str = eq_split[0]
-        value_str = eq_split[1]
-
-        # Pull the values out of the string. This is crude, but works.
-        idx = [int(x) for x in
-               re.split(',',
-                        re.match('(?:Ybus\()(.*)(?:\))', idx_str).group(1))]
-
-        # Not working with diagonals.
-        if idx[0] == idx[1]:
-            continue
-
-        # Remove parentheses from the value_str.
-        value_str = re.sub('(?:\()(.*)(?:\))', lambda m: m.group(1), value_str)
-
-        # Split by the '+j*'
-        val_list = [float(x) for x in re.split('\+j\*', value_str)]
-
-        # Create complex number, invert and multiply by -1 to convert
-        # admittance to impedance.
-        try:
-            cplx = -1 / (val_list[0] + 1j * val_list[1])
-        except ZeroDivisionError:
-            # Move along if the buses are not connected.
-            continue
-
-        # Extract the reactance.
-        x = cplx.imag
-
-        # Add to the graph.
-        g.add_edge(idx[0], idx[1], x=x)
-
-    return g
 
 
 if __name__ == '__main__':
